0570300ed45d1e676a9d882045d0955bc3bdd3e7
Alexander Missaoui <alexandermissaoui@gmail.com>
2023-10-24 14:20:40 +0200

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 9999
+const PORT = process.env.PORT || 7777
 
 app.listen(PORT, () => console.log('server running on ' + PORT))
 
