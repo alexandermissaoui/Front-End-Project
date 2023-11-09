f2a97cec7e1cc7db0cade4fd33ae662f4c3ede0f
Alexander Missaoui <alexandermissaoui@gmail.com>
2023-11-09 12:47:33 +0100

    reservation style

--- Front-End/src/Pages/Reservations/Reservations.jsx
+++ Front-End/src/Pages/Reservations/Reservations.jsx
@@ -1,36 +1,34 @@
-import { object } from 'prop-types';
 import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import { useUser } from '../../Context/UserContext';
 
 const Reservations = () => {
   const [reservations, setReservations] = useState([]);
-  const userState = useUser()
+  const userState = useUser();
 
   useEffect(() => {
-    if(userState.token){
-      console.log(userState);
-    // Fetch user reservations from the API endpoint
-    fetch('http://localhost:3030/api/reservations/user/me', {
-
-      headers: {
-        'Content-Type': 'application/json',
-        // Add authorization header with user token if needed
-        Authorization: `Bearer ${userState.token}`
-      }
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("RESERVATIONS: ", data)
-        setReservations(data)
-      }) // Assuming the API response is an array of reservation objects
-      .catch((error) => console.error('Error fetching reservations:', error));
+    if (userState.token) {
+      // Fetch user reservations from the API endpoint
+      fetch('http://localhost:3030/api/reservations/user/me', {
+        headers: {
+          'Content-Type': 'application/json',
+          // Add authorization header with user token if needed
+          Authorization: `Bearer ${userState.token}`
+        }
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("RESERVATIONS: ", data)
+          setReservations(data)
+        }) // Assuming the API response is an array of reservation objects
+        .catch((error) => console.error('Error fetching reservations:', error));
     }
   }, [userState.token]); // Empty dependency array ensures the effect runs once after the initial render
+
   return (
-    <>
+    <div className="d-flex flex-column align-items-center mt-4 mb-4">
       {reservations.map((reservation, index) => (
-        <Card key={index} style={{ width: '18rem', marginBottom: '10px' }}>
+        <Card key={index} className="w-75 mb-3">
           <Card.Body>
             <Card.Title>{reservation.accommodation.title}</Card.Title>
             <Card.Text>
@@ -39,7 +37,7 @@ const Reservations = () => {
           </Card.Body>
         </Card>
       ))}
-    </>
+    </div>
   );
 };
 
