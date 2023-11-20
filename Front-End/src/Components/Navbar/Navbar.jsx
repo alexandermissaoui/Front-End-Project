39a814641ee448420e49f7ceba94c953fbc52210
Alexander Missaoui <alexandermissaoui@gmail.com>
2023-11-20 16:14:32 +0100

    styling done

--- Front-End/src/Components/Navbar/Navbar.jsx
+++ Front-End/src/Components/Navbar/Navbar.jsx
@@ -3,16 +3,16 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useUser } from '../../Context/UserContext'; // Importera useUser-hooket
+import { useUser } from '../../Context/UserContext';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './navbar.css';
-
+import { faCircleUser, faHamburger } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function NavbarComponent() {
-  const user = useUser(); // Använd useUser-hooket för att få åtkomst till användartoken
+  const user = useUser();
 
   const handleLogout = () => {
-    // Rensa användartoken vid utloggning
     localStorage.removeItem("TOKEN");
     user.setToken(null);
   };
@@ -28,13 +28,12 @@ function NavbarComponent() {
             <Nav.Link href="/About">About</Nav.Link>
             <Nav.Link href="/Reservations">Reservations</Nav.Link>
             <Nav.Link href="/Support">Support</Nav.Link>
-
+          </Nav>
+          <Nav className="ml-auto"> {/* Use ml-auto class here */}
             {user.token ? (
-
               <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
             ) : (
-              
-              <NavDropdown title="Login" id="basic-nav-dropdown" className='dropdown'>
+              <NavDropdown className='hamburger-icon' title={<span><FontAwesomeIcon icon={faHamburger} style={{ color: '#000000' }} /> <FontAwesomeIcon icon={faCircleUser} style={{ color: '#000000' }} /></span>}>
                 <NavDropdown.Item href="/Login">Login</NavDropdown.Item>
                 <NavDropdown.Item href="/Register">Register</NavDropdown.Item>
               </NavDropdown>
